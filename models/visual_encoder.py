c7e2a846a8ef1b38ae67163b236e8bb5f444eb15
rentainhe <596106517@qq.com>
2022-04-21 09:29:30 +0800

    add LICENSE for models

--- models/visual_encoder.py
+++ models/visual_encoder.py
@@ -1,12 +1,28 @@
+# coding=utf-8
+# Copyright 2022 The SimREC Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warnings
+
+import torch
+import torch.nn as nn
+from torch.backends import cudnn
+
 from utils.parse_darknet_weights import parse_yolo_weights
 from layers.conv_layer import *
 from torchvision.models.resnet import resnet34,resnet101,resnet18
 from utils.ResNetD import ResNetV1c
-
-import torch
-import torch.nn as nn
-import warnings
-from torch.backends import cudnn
 
 class CspDarkNet(nn.Module):
     def __init__(self, __C,multi_scale_outputs=False):
