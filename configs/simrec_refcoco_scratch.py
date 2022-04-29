d384c4cf804e7ab72ba87db7de5794d6db419c49
rentainhe <596106517@qq.com>
2022-04-29 00:32:01 +0000

    update log metric

--- configs/simrec_refcoco_scratch.py
+++ configs/simrec_refcoco_scratch.py
@@ -5,12 +5,18 @@ from .common.optim import optim
 from .common.models.simrec import model
 
 
+# Refine data path depend your own need
+dataset.ann_path["refcoco"] = "/home/rentianhe/dataset/rec/anns/refcoco.json"
+dataset.image_path["refcoco"] = "/home/rentianhe/dataset/rec/images/train2014"
+dataset.mask_path["refcoco"] = "/home/rentianhe/dataset/rec/masks/refcoco"
+
 # # Refine training cfg
-train.output_dir = "./output"
+train.output_dir = "./output/sync_with_no_sequential"
 train.batch_size = 32
 train.save_period = 1
 train.log_period = 10
 train.evaluation.eval_batch_size = 32
+train.sync_bn.enabled = True
 
 # Refine model cfg
 model.visual_backbone.pretrained = True
